61c6588ab258e2a1958ab7e3af143f7bc215d918
Nick Semenkovich <semenko@alum.mit.edu>
2014-04-07 18:01:19 -0500

    Stub create -- switch to bash since this is silly easy.

---
+++ code-parsing/code-to-json.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Convert ICD-10 codes to a sane JSON format.
+#
+# Copyright (c) ClinDesk, Inc.
+# Author: 2014 Nick Semenkovich <semenko@alum.mit.edu>.
+#   https://nick.semenkovich.com/
+#
+
