f1f02c45368e9fbf9271a04e653841ea8efd188c
Nick Semenkovich <semenko@alum.mit.edu>
2014-04-06 21:55:19 -0500

    Add a stub python file for parsing.

---
+++ code-parsing/parse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Parse ICD-10 codes into some sane JSON format.
+#
+#
+# Copyright (c) ClinDesk, Inc.
+# Author: 2014 Nick Semenkovich <semenko@alum.mit.edu>.
+#   https://nick.semenkovich.com/
+#
+# This software is released under the MIT License:
+#  <http://www.opensource.org/licenses/mit-license.php>
+#
+
+from __future__ import absolute_import, division, print_function, unicode_literals
