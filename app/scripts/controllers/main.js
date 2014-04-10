da6d997ad02386c54f7e64262e65dd57b7d4a9e6
Nick Semenkovich <semenko@alum.mit.edu>
2014-04-09 23:02:44 -0500

    Update code parsing & filtering.
    Switch to ng-if (vs ng-show) so DOM doesn't explode (!)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,10 @@
 'use strict';
 
 angular.module('icd10App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.query = "B10.8"
-    $scope.codes = [
-      {"code":"B10.81", "text":"Human herpesvirus 6 infection", "description":"Infection by human herpesvirus 6"},
-      {"code":"B10.82", "text":"Human herpesvirus 7 infection", "description":"Infection by human herpesvirus 7"},
-      {"code":"B10.89", "text":"Other human herpesvirus infection", "description":"Applicable to human herpesvirus 8 infection or Kaposi's sarcoma-associated herpesvirus infection"}
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.query = "B12"
+        $http.get('temp-codes.json')
+            .then(function(res){
+                $scope.codes = res.data;
+            });
+    });
