871e7a02b0ae8a56c129650cfc424a69f2cd9940
Leo Shmuylovich <leo@noredink.com>
2014-04-13 15:00:44 -0500

    rework filter to only be called once through a . on the query, all handled in the controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,28 @@
 'use strict';
 
 angular.module('icd10App')
-    .controller('MainCtrl', function ($scope, CodeRetriever) {
-        $scope.query = "hel";
+    .controller('MainCtrl', function ($scope, CodeRetriever, $filter) {
+        $scope.query = "";
         $scope.currentLimit = 20;
         $scope.codes = [];
         $scope.filtered = [];
+
+
         CodeRetriever.get().then(function(data){
           $scope.codes = data;
         });
+        // get the filter steps into controller,
+        // so tht it can be controlled by a watcher
+        // and not be defined by the ng-repeat digest cycle
+        var filterFilter = $filter('andFilter');
+        var limitToFilter = $filter('limitTo');
+        // may want to orderBy eventually, here is the filter
+        var orderByFilter = $filter('orderBy');
+        $scope.search_times = [0,0];
+        $scope.$watch('query', function(newVal, oldVal){
+          $scope.search_times[0] = Date.now();
+          var tmp = filterFilter($scope.codes, $scope.query)
+          $scope.filtered = limitToFilter(tmp, $scope.currentLimit);
+          $scope.search_times[1] = Date.now();
+        })
     });
