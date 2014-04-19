cbe6368fd877c34eaaf9d8c19b4da74dd01a6e04
Nick Semenkovich <semenko@alum.mit.edu>
2014-04-19 13:23:49 -0500

    Directive working, cool! Will need to think more about the UI/UX.

--- app/scripts/directives/codeChildren.js
+++ app/scripts/directives/codeChildren.js
@@ -3,10 +3,17 @@
 angular.module('icd10App')
   .directive('codeChildren', function () {
     return {
-      template: '<td><ul><li ng-repeat="m in code.m">{{m}}</li></ul></td><td><ul><li ng-repeat="m in code.m">lookup {{m}} in children json</li></ul></td>',
+      template: '<td><ul><li ng-repeat="m in code.m">{{m}}</li></ul></td><td><ul><li ng-repeat="d in children track by $index">{{d}}</li></ul></td>',
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
+        var children = [];
 
+        var arrayLength = scope.code.m.length;
+        for (var i = 0; i < arrayLength; i++) {
+            children.push(scope.dx_children[scope.code.m[i]]);
+        }
+
+        scope.children = children;
       }
     };
   });
