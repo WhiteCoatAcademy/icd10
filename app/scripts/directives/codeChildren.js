9cb6688d500463e3e66f9b00914b29a775360e16
Leo Shmuylovich <leo@noredink.com>
2014-04-19 16:21:40 -0500

    simplify the code children directive, the toggling, and add in spinners based on scope variables that are tied to data loading callbacks

--- app/scripts/directives/codeChildren.js
+++ app/scripts/directives/codeChildren.js
@@ -3,17 +3,9 @@
 angular.module('icd10App')
   .directive('codeChildren', function () {
     return {
-      template: '<td><ul><li ng-repeat="m in code.m">{{m}}</li></ul></td><td><ul><li ng-repeat="d in children track by $index">{{d}}</li></ul></td>',
+      templateUrl: 'views/code_children.html',
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
-        var children = [];
-
-        var arrayLength = scope.code.m.length;
-        for (var i = 0; i < arrayLength; i++) {
-            children.push(scope.dx_children[scope.code.m[i]]);
-        }
-
-        scope.children = children;
       }
     };
   });
