1d2457da6b75869a03698dab3846d72c026a5014
Leo Shmuylovich <leo@noredink.com>
2014-04-27 12:34:42 -0500

    our filter can filter by keys that we pass in an array

--- app/scripts/filters/andFilter.js
+++ app/scripts/filters/andFilter.js
@@ -2,15 +2,29 @@
 
 angular.module('icd10App')
   .filter('andFilter', function (filterFilter) {
-    return function (searchArray, queryString) {
-      var results = searchArray;
+    return function (searchArray, queryString, filterByKeysArray) {
+      var resultsArrayByKey = {};
+      var results = [];
+      angular.forEach(filterByKeysArray, function(key){
+        resultsArrayByKey[key] = searchArray;
+      });
+
       if(queryString){
         var queryParts = queryString.split(' ');
-        angular.forEach(queryParts, function(value, key){
-          // TODO: make this modular.
-          results = filterFilter(results, {'k': value});
+        angular.forEach(queryParts, function(value){
+          angular.forEach(filterByKeysArray, function(key){
+            var queryObject = {};
+            queryObject[key] = value;
+            resultsArrayByKey[key] = filterFilter(resultsArrayByKey[key], queryObject);
+          })
         });
       }
+      angular.forEach(resultsArrayByKey, function(value){
+        results = _.union(results, value);
+
+      });
+
+
       return results;
     };
   });
