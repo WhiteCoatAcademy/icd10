b56049f84f8be7cbc7e822f13fb6b21ef3f06fb0
Leo Shmuylovich <leo@noredink.com>
2014-04-27 12:36:07 -0500

    use the data from search preferences to specify what we are searching against. Also fix a bug where search was occuring despite the query being too short

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,14 @@ angular.module('icd10App')
         $scope.codes = [];
         $scope.totalResults = 0;
         $scope.filtered = [];
-        console.log($routeParams)
+
+        $scope.searchPreferences = {
+          codeParent: {key: 'c', isSearching:false},
+          codeChildren: {key:'m', isSearching: false},
+          description: {key:'d', isSearching: true},
+          keyword: {key:'k', isSearching: true}
+        }
+
         // we are binding to scope variables here, but this kind of thing can be handled
         // with $broadcast of loading events. That is better if there are multiple controllers
         // or if we want global awareness of loading
@@ -38,7 +45,7 @@ angular.module('icd10App')
 
         $scope.search_times = [0,0];
         $scope.$watch('query', function(newVal, oldVal){
-          $scope.updateResults(newVal, oldVal);
+          $scope.respondToQueryChange(newVal, oldVal);
           // Send event to GA
           if(newVal !== oldVal && newVal !== '') {
             ga('send', 'event', 'q', newVal);
@@ -48,14 +55,28 @@ angular.module('icd10App')
         if (!Date.now) {
             Date.now = function() { return new Date().getTime(); };
         }
-        $scope.updateResults = function(newVal, oldVal){
+        $scope.$watch('searchPreferences', function(){
+          $scope.updateSearchKeysArray();
+        }, true)
+
+        $scope.updateSearchKeysArray = function(){
+          $scope.filterKeysArray = _.pluck(_.filter($scope.searchPreferences, function(value){return value.isSearching}), 'key');
+          $scope.updateResults(boringWordsFilter($scope.query));
+        }
+
+        $scope.updateResults = function(cleanQuery){
+          $scope.search_times[0] = Date.now();
+          console.log($scope.filterKeysArray);
+          $scope.filtered = andFilter($scope.codes, cleanQuery, $scope.filterKeysArray);
+          $scope.search_times[1] = Date.now();
+          $location.search('q', cleanQuery);
+        }
+        $scope.respondToQueryChange = function(newVal, oldVal){
           var cleanQuery = boringWordsFilter(newVal);
           // only do the filter if the filtered query (without boring words)
-          if(cleanQuery !== boringWordsFilter(oldVal)){
-              $scope.search_times[0] = Date.now();
-              $scope.filtered = andFilter($scope.codes, cleanQuery);
-              $scope.search_times[1] = Date.now();
-              $location.search('q', cleanQuery);
+          var isValidQuery = cleanQuery.length>2 &&(cleanQuery !== boringWordsFilter(oldVal))
+          if(isValidQuery){
+            $scope.updateResults(cleanQuery);
           }
         }
     });
