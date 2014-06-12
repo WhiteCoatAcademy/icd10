a8f70ecba12c5c51352f49dcd71c2d80b50ebd16
Leo Shmuylovich <leoshmu@gmail.com>
2014-06-12 14:12:05 -0500

    rename variable to be clearer as to use of highligh to match exact phrases vs to match by word

--- app/scripts/filters/highlight.js
+++ app/scripts/filters/highlight.js
@@ -4,23 +4,23 @@
  * Wraps the
  * @param text {string} haystack to search through
  * @param search {string} needle to search for
- * @param [caseSensitive] {boolean} optional boolean to use case-sensitive searching
+ * @param [isMatchExact] {boolean} optional boolean to use exact searching searching
  */
 angular.module('icd10App')
 .filter('highlight', function () {
-  return function (text, search, caseSensitive, cssClass) {
+  return function (text, search, isMatchExact, cssClass) {
     cssClass = cssClass || 'ui-match';
     if (text && (search || angular.isNumber(search))) {
       text = text.toString();
       search = search.toString();
-      if (caseSensitive) {
+      if (isMatchExact) {
         return text.split(search).join('<span class="'+cssClass+'">' + search + '</span>');
       } else {
-
         _.each(search.split(' '), function(word){
           if(word.length>1){
             text = text.replace(new RegExp(word, 'gi'), '####$&????');
           }
+          // console.log(text)
           text = text.replace('####', '<span class="'+cssClass+'">');
           text = text.replace('????', '</span>');
         })
